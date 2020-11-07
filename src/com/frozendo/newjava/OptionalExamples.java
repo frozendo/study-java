52471551a81dbade01308fe3ceb0642be21e3922
Flavio Rozendo <flavio.ap.rozendo@gmail.com>
2020-11-07 17:20:36 -0300

    Optional examples

---
+++ src/com/frozendo/newjava/OptionalExamples.java
@@ -0,0 +1,151 @@
+package com.frozendo.newjava;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public class OptionalExamples {
+
+    public static void main(String[] args) {
+        Optional<String> opt = Optional.of("Optional string with of");
+        Optional<String> optNotNullable = Optional.ofNullable("Opional string with ofNullable");
+        Optional<String> optNullable = Optional.ofNullable(null);
+        Optional<String> empty = Optional.empty();
+
+        //test isPresent method
+        System.out.println("*** isPresent result ***");
+        useIsPresent(opt);
+        useIsPresent(optNotNullable);
+        useIsPresent(optNullable);
+        useIsPresent(empty);
+
+        System.out.println();
+
+        //test ifPresent method
+        System.out.println("*** ifPresent result ***");
+        useIfPresent(opt);
+        useIfPresent(optNotNullable);
+        useIfPresent(optNullable);
+        useIfPresent(empty);
+
+        System.out.println();
+
+        //test filter
+        System.out.println("*** filter result ***");
+        optionalFilter(opt);
+        optionalFilter(optNotNullable);
+        optionalFilter(optNullable);
+        optionalFilter(empty);
+
+        System.out.println();
+
+        //test map
+        System.out.println("*** map result ***");
+        optionalMap(opt);
+        optionalMap(optNotNullable);
+        optionalMap(optNullable);
+        optionalMap(empty);
+
+        System.out.println();
+
+        //test flatMap
+        System.out.println("*** flatMap result ***");
+        optionalFlatMap(opt);
+        optionalFlatMap(optNotNullable);
+        optionalFlatMap(optNullable);
+        optionalFlatMap(empty);
+
+        System.out.println();
+
+        //test orElse
+        System.out.println("*** orElse result ***");
+        optionalOrElse(opt);
+        optionalOrElse(optNotNullable);
+        optionalOrElse(optNullable);
+        optionalOrElse(empty);
+
+        System.out.println();
+
+        //test orElesGet
+        System.out.println("*** orElesGet result ***");
+        optionalOrElseGet(opt);
+        optionalOrElseGet(optNotNullable);
+        optionalOrElseGet(optNullable);
+        optionalOrElseGet(empty);
+
+        System.out.println();
+
+        //test orElesGetThrow
+        System.out.println("*** orElesGetThrow result ***");
+        optionalOrElseThrow(opt);
+        optionalOrElseThrow(optNotNullable);
+        optionalOrElseThrow(optNullable);
+        optionalOrElseThrow(empty);
+    }
+
+    public static void useIsPresent(Optional<String> optional) {
+        if (optional.isPresent()) {
+            System.out.println("Value is: " + optional.get());
+        } else {
+            System.out.println("Optional is empty!");
+        }
+    }
+
+    public static void useIfPresent(Optional<String> optional) {
+        optional.ifPresent(s -> System.out.println("Value with ifPresent: " + optional.get()));
+    }
+
+    public static void optionalFilter(Optional<String> optional) {
+        Optional<String> optString = optional.filter(s -> Objects.nonNull(s) && !s.isEmpty());
+        useIsPresent(optString);
+    }
+
+    public static void optionalMap(Optional<String> optional) {
+        Optional<Integer> optMap = optional.map(s -> s.length());
+        if (optMap.isPresent()) {
+            System.out.println("Map result: " + optMap.get());
+        } else {
+            System.out.println("Length is 0!");
+        }
+    }
+
+    public static void optionalFlatMap(Optional<String> optional) {
+        Optional<Integer> optFlatMap = optional.flatMap(s -> {
+            if (Objects.nonNull(s) && !s.isEmpty()) {
+                return Optional.of(s.length());
+            }
+            return Optional.empty();
+        });
+
+        if (optFlatMap.isPresent()) {
+            System.out.println("FlatMap result: " + optFlatMap.get());
+        } else {
+            System.out.println("Length is 0!");
+        }
+    }
+
+    public static void optionalOrElse(Optional<String> optional) {
+        String test = optional
+                .filter(s -> Objects.nonNull(s) && !s.isEmpty())
+                .orElse("Optional empty, execute orElse");
+        System.out.println(test);
+    }
+
+    public static void optionalOrElseGet(Optional<String> optional) {
+        String test = optional
+                .filter(s -> Objects.nonNull(s) && !s.isEmpty())
+                .orElseGet(String::new);
+        System.out.println(test.isEmpty() ? "Optional is empty, orElseGet executed" : test);
+    }
+
+    public static void optionalOrElseThrow(Optional<String> optional) {
+        try {
+            String test = optional
+                    .filter(s -> Objects.nonNull(s) && !s.isEmpty())
+                    .orElseThrow(RuntimeException::new);
+            System.out.println(test);
+        } catch(Exception e) {
+            System.out.println("Optional is empty, orElseThrow executed");
+        }
+    }
+
+}
